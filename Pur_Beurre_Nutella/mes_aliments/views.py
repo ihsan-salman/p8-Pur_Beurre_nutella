a4e8d5536d3aeb49c7441c43a0e0d1ec739de972
ihsan-salman <ihsan.salmantr@gmail.com>
2021-06-02 11:42:11 +0000

    html coding

--- Pur_Beurre_Nutella/mes_aliments/views.py
+++ Pur_Beurre_Nutella/mes_aliments/views.py
@@ -1,8 +1,9 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
-def index(requests):
-	message = 'salut tout le monde'
-	return HttpResponse(message)
+def index(request):
+	template = loader.get_template('mes_aliments/index.html')
+	return HttpResponse(template.render(request=request))
