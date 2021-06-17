459b8313562cc5a42bfb0b8de62bae9c8bbdf074
ihsan-salman <ihsan.salmantr@gmail.com>
2021-06-17 16:13:17 +0000

    insert data into DB

--- Pur_Beurre_Nutella/mes_aliments/psql.py
+++ Pur_Beurre_Nutella/mes_aliments/psql.py
@@ -13,18 +13,18 @@ class Psql_data:
                               'dessert', 'salade', "cookie"]
         self.url = "https://fr.openfoodfacts.org/cgi/search.pl?"
         self.product_categorie = []
-        self.input_data = 0
+        self.category_data = []
         self.insert_category_name()
-        #self.get_data_openfoodfacts()
-        #self.insert_product_data()
+        self.get_data_openfoodfacts()
+        self.insert_product_data()
 
     def insert_category_name(self):
         """Save the Category's name in the database"""
         if Category.objects.count() != 5:
             for name in self.category_name:
                 self.input_data = Category(name=name)
-                print(self.input_data)
                 self.input_data.save()
+
 
     def get_data_openfoodfacts(self):
         """Get data from the API of OpenFoodFacts"""
@@ -42,13 +42,12 @@ class Psql_data:
             self.json_category = self.json_request.json()
             # Add all json data in a list to make easier the usage
             self.product_categorie.append(self.json_category)
-        self.category = Category.objects.all()
-        print(self.category[0])
+
     def insert_product_data(self):
         """Insert and Save the data of each product in the database"""
+        self.input_data = []
         if Product.objects.count() != 500:
             for i in range(len(self.category_name)):
-                print(i)
                 for product in self.product_categorie[i]['products']:
                     self.name = product.get('product_name')
                     self.brands = product.get('brands'),
@@ -61,5 +60,5 @@ class Psql_data:
                         nutriscore_grade=self.nutriscore_grade,
                         url=self.url,
                         stores=self.stores,
-                        category=i)
+                        category=Category.objects.get(name=self.category_name[i]))
                     self.input_data_product.save()
