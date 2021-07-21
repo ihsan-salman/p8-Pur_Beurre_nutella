d54f1b85c04c21d61edfa4c17ec19febdad9fb9a
ihsan-salman <ihsan.salmantr@gmail.com>
2021-07-21 16:07:42 +0200

    index page status_code test and start other tests

--- Pur_Beurre_Nutella/mes_aliments/tests.py
+++ Pur_Beurre_Nutella/mes_aliments/tests.py
@@ -2,6 +2,17 @@
    -*- coding: Utf-8 -'''
 
 
+from django.urls import reverse
 from django.test import TestCase
 
+from .models import Category, Contact, Product, Favorite
+
 #Create your tests here
+
+class IndexPageTestCase(TestCase):
+   def test_index_page(self):
+      response  = self.client.get(reverse('home'))
+      self.assertEqual(response.status_code, 200)
+
+class ProductPageTestCase(TestCase):
+   def test_product_page_returns_200(self):
