fd78b1b91c62313395d2c2a46ebff1aec73e807e
ihsan-salman <ihsan.salmantr@gmail.com>
2021-07-27 17:35:01 +0200

    account page test with client login

--- Pur_Beurre_Nutella/mes_aliments/tests.py
+++ Pur_Beurre_Nutella/mes_aliments/tests.py
@@ -4,6 +4,7 @@
 
 from django.urls import reverse
 from django.test import TestCase
+from django.test import Client
 from django.contrib.auth.models import User
 
 from .models import Category, Contact, Product, Favorite
@@ -11,33 +12,57 @@ from .models import Category, Contact, Product, Favorite
 #Create your tests here
 
 class IndexPageTestCase(TestCase):
-   def test_index_page(self):
-      response  = self.client.get(reverse('home'))
-      self.assertEqual(response.status_code, 200)
+    '''Index page test class'''
+    def test_index_page_returns_200(self):
+        '''Test if the Http request returns 200'''
+        response  = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
 
-'''class ProductPageTestCase(TestCase):
-   def test_product_page_returns_200(self):
-      pizza = Category.objects.create(name="pizza")'''
-
-class AccountPageTestCase(TestCase):
-   def test_account_page_returns_200(self):
-      response = self.client.get(reverse('mon_compte'))
-      self.assertEqual(response.status_code, 200)
+class ProductPageTestCase(TestCase):
+    '''Product page test class'''
+    def setUp(self):
+        '''Init all needed data for the test
+           In that case, create new products is necessary'''
+        pizza = Category.objects.create(name="pizza")
+        boisson = Category.objects.create(name="boisson")
+    def test_product_page_returns_200(self):
+        '''Test if the Http request returns 200
+           and all substitute with a best nutriscore
+           for the selected product '''
+        pass
 
 
 class LoginPageTestCase(TestCase):
+    '''Login page test class'''
     def test_login_page_returns_200(self):
+        '''Test of the Http request returns 200'''
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
-
     def setUp(self):
+        '''Init all needed data to test the user's login'''
         self.credentials = {
             'username': 'testuser',
             'password': 'secret'}
         User.objects.create_user(**self.credentials)
     def test_login(self):
+        '''Test if the user is logged after the login step'''
         # send login data
         response = self.client.post('/login/', self.credentials, follow=True)
         # should be logged in now
         self.assertTrue(response.context['user'].is_active)
-      
+
+
+class AccountPageTestCase(TestCase):
+    '''Account page test class'''
+    def setUp(self):
+        '''Init all needed data for the test'''
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        '''Test if the Http request returns 200 when the user is logged'''
+        # send login data
+        self.client.post('/login/', self.credentials, follow=True)
+        response = self.client.get('/mon_compte/')
+        self.assertEqual(response.status_code, 200)
